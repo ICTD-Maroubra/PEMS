2f8ac957287e96ce1704bbf5a1fe88ced85d828f
Kutlu Omeroglu <kutluomeroglu@gmail.com>
2017-10-12 23:46:55 +1100

    Revert "Started working on mongo-pems service"
    This reverts commit 5204c8104849b3df6ffd6a01118393526530f1f1.

--- pems-server/src/main/java/org/maroubra/pemsserver/configuration/MongoDbConfiguration.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/configuration/MongoDbConfiguration.java
@@ -5,6 +5,6 @@ import com.mongodb.ConnectionString;
 public class MongoDbConfiguration {
 
     public ConnectionString getConnectionString() {
-        return new ConnectionString("mongodb://127.0.0.1:27017/local");
+        return new ConnectionString("");
     }
 }
