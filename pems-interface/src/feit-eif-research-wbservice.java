1fac553af67e7cd50d428b15f694273f29f56ca6
alberty4573 <shenlieku@gmail.com>
2017-10-14 17:18:43 +1100

    Added getSensorData method

--- pems-interface/src/feit-eif-research-wbservice.java
+++ pems-interface/src/feit-eif-research-wbservice.java
@@ -7,6 +7,16 @@ import java.net.URL;
 import java.net.URLConnection;
 public class TestWbService {
 
+	private static String URL_BASE = 'http://eif-research.feit.uts.edu.au/api/json/?';
+
+	private String rFromDate;
+	private String rToDate;
+	private String rFamily;
+	private String rSensor;
+	private String rSubsensor;
+
+	public TestWbService() {}
+
 	public static void main(String args[])
 	{
 		BufferedReader rd;
@@ -14,7 +24,7 @@ public class TestWbService {
 
 		try
 		{
-			//dsadsadsa
+
 			URL url = new URL("http://eif-research.feit.uts.edu.au/api/json/?rFromDate=2017-10-10T01%3A59%3A38&rToDate=2017-10-10T12%3A22%3A38&rFamily=weather&rSensor=AT");
 			URLConnection conn = url.openConnection();
 			conn.setDoOutput(true);
@@ -31,5 +41,90 @@ public class TestWbService {
 		catch (Exception e) {
 			System.out.println(e.toString());
 		}
+	}
+
+	public String getSensorData () {
+
+		String url;
+		url = URL_BASE + "rFromDate=" + rFromDate + "&" + "rToDate=" + rToDate + "&" ;
+
+		if (this.rFamily.equals("weather")) {
+			url += "rFamily=" + "weather" + "&" + "rSensor=" + rSensor;
+
+		}
+
+		else if (this.rFamily.equals("wasp")) {
+			url += "rFamily=" + "wasp" + "&" + "rSensor=" + rSensor + "&" + "rSubsensor" + rSubsensor;
+		}
+
+		else {
+			// invalid sensor type
+		}
+
+		BufferedReader rd;
+		OutputStreamWriter wr;
+
+		try
+		{
+
+			URL url = new URL(url);
+			URLConnection conn = url.openConnection();
+			conn.setDoOutput(true);
+			wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.flush();
+
+			// Get the response
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			String result;
+			while ((line = rd.readLine()) != null) {
+				result += line;
+			}
+			return result;
+		}
+		catch (Exception e) {
+			//
+		}
+
+	}
+
+	public String getrFromDate() {
+		return rFromDate;
+	}
+
+	public void setrFromDate(String rFromDate) {
+		this.rFromDate = rFromDate;
+	}
+
+	public String getrToDate() {
+		return rToDate;
+	}
+
+	public void setrToDate(String rToDate) {
+		this.rToDate = rToDate;
+	}
+
+	public String getrFamily() {
+		return rFamily;
+	}
+
+	public void setrFamily(String rFamily) {
+		this.rFamily = rFamily;
+	}
+
+	public String getrSensor() {
+		return rSensor;
+	}
+
+	public void setrSensor(String rSensor) {
+		this.rSensor = rSensor;
+	}
+
+	public String getrSubsensor() {
+		return rSubsensor;
+	}
+
+	public void setrSubsensor(String rSubsensor) {
+		this.rSubsensor = rSubsensor;
 	}
 }
