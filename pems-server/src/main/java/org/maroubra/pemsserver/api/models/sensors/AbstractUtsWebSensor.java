36edf2d062a8ac580cbdf4ed2f96be50e655c06e
Kutlu Omeroglu <kutluomeroglu@gmail.com>
2017-10-15 12:41:14 +1100

    Fixed query builder, working on request.

--- pems-server/src/main/java/org/maroubra/pemsserver/api/models/sensors/AbstractUtsWebSensor.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/api/models/sensors/AbstractUtsWebSensor.java
@@ -1,47 +1,87 @@
 package org.maroubra.pemsserver.api.models.sensors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.net.URLConnection;
+
 
 public class AbstractUtsWebSensor {
-    protected Date fromDate;
-    protected Date toDate;
+    protected LocalDateTime fromDate;
+    protected LocalDateTime toDate;
+    protected int pollIntervalMinutes = 60;
     protected String family;
     protected String subSensor;
     protected String sensor;
     protected String apiLink = "https://eif-research.feit.uts.edu.au/api/";
     protected String dataFormat = "json/";
     protected String query = null;
+    private static Logger log = LoggerFactory.getLogger(AbstractUtsWebSensor.class);
 
 
-    private String buildHTTPQuery()
+    public AbstractUtsWebSensor(String family, String sensor, String subSensor, int pollIntervalMinutes)
     {
-        Map<String,String> parameters = new HashMap<>();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-M-yyyy'T'hh:mm:ss");
+        this.family = family;
+        this.subSensor = subSensor;
+        this.sensor = sensor;
+        this.pollIntervalMinutes = pollIntervalMinutes;
+    }
 
-        parameters.put("rFromDate", dateFormat.format(fromDate));
-        parameters.put("rToDate",dateFormat.format(toDate));
+    public void setPollIntervalMinutes(int pollIntervalMinutes) {
+        this.pollIntervalMinutes = pollIntervalMinutes;
+    }
+
+    public void setDates() {
+        fromDate = LocalDateTime.now().minusMinutes(60).withNano(0);
+        toDate = LocalDateTime.now().withNano(0);
+    }
+
+    public String getData()
+    {
+        String content = null;
+        try {
+            log.info(query);
+            URL url = new URL(query);
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            content = conn.getContent().toString();
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return content;
+    }
+
+    public String buildHTTPQuery() {
+        String partialQuery = "";
+        Map<String,String> parameters = new LinkedHashMap<>();
+        parameters.put("rFromDate",fromDate.toString());
+        parameters.put("rToDate",toDate.toString());
         parameters.put("rFamily",family);
         parameters.put("rSensor",sensor);
         parameters.put("rSubSensor", subSensor);
 
         try {
-            query = QueryBuilder.QueryBuilder(parameters);
+            partialQuery = QueryBuilder.QueryBuilder(parameters);
         }
         catch (UnsupportedEncodingException e){
 
         }
+        query = apiLink + "" + dataFormat + "" + partialQuery;
         return query;
     }
 
 
 
 
-    //https://eif-research.feit.uts.edu.au/api/json/?rFromDate=2017-10-13T09%3A57%3A18&rToDate=2017-10-15T09%3A57
-    // %3A18&rFamily=wasp&rSensor=ES_B_11_429_3E90&rSubSensor=BAT
 
 
 
--- pems-server/src/main/java/org/maroubra/pemsserver/api/models/sensors/QueryBuilder.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/api/models/sensors/QueryBuilder.java
@@ -8,12 +8,8 @@ public class QueryBuilder {
     public static String QueryBuilder(Map<String, String> params)
             throws UnsupportedEncodingException {
         StringBuilder result = new StringBuilder();
-
-        //?rFromDate=2017-10-13T09%3A57%3A18
-        // &rToDate=2017-10-15T09%3A57%3A18
-        for (Map.Entry<String, String> entry : params.entrySet()) {
             result.append("?");
-            //rFromDate
+        for (Map.Entry<String, String> entry : params.entrySet()) {
             result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
             result.append("=");
             result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
