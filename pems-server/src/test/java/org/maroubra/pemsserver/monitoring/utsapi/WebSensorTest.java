aa3d432bd1d7d7b6a67250378c3ed4e96d5ee414
Kutlu Omeroglu <kutluomeroglu@gmail.com>
2017-10-23 21:13:41 +1100

    how about now.

--- pems-server/src/test/java/org/maroubra/pemsserver/monitoring/utsapi/WebSensorTest.java
+++ pems-server/src/test/java/org/maroubra/pemsserver/monitoring/utsapi/WebSensorTest.java
@@ -11,19 +11,12 @@ import static com.google.common.truth.Truth.assertThat;
 public class WebSensorTest {
 
 
+    @Test
     public void testWebSensorStart() {
 
         WebSensor.Config webSensorConfig1 = new WebSensor.Config();
         webSensorConfig1.setId("Web001");
         webSensorConfig1.setConfig("wasp","ES_B_05_416_7C15","BAT");
-
-        WebSensor.Config webSensorConfig2 = new WebSensor.Config();
-        webSensorConfig2.setId("Web002");
-        webSensorConfig2.setConfig("wasp","ES_B_11_429_3E90","BAT");
-
-        WebSensor.Config webSensorConfig3 = new WebSensor.Config();
-        webSensorConfig3.setId("Web003");
-        webSensorConfig3.setConfig("wasp","ES_B_11_429_3E90","TCA");
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://eif-research.feit.uts.edu.au/api/")
@@ -37,26 +30,6 @@ public class WebSensorTest {
         SensorLog createdLog = webSensor1.logs().blockingFirst();
 
         if (createdLog != null) {
-            assertThat(createdLog.getSensorId()).matches(webSensorConfig1.getId());
-            assertThat(createdLog.getAttributeValue()).hasSize(1);
-        }
-
-        WebSensor webSensor2 = new WebSensor(webSensorConfig2, webApi);
-        webSensor2.setPollIntervalMinutes(360);
-        webSensor2.start();
-        SensorLog createdLog2 = webSensor2.logs().blockingFirst();
-
-        if (createdLog2 != null) {
-            assertThat(createdLog.getSensorId()).matches(webSensorConfig1.getId());
-            assertThat(createdLog.getAttributeValue()).hasSize(1);
-        }
-
-        WebSensor webSensor3 = new WebSensor(webSensorConfig3, webApi);
-        webSensor3.setPollIntervalMinutes(360);
-        webSensor3.start();
-        SensorLog createdLog3 = webSensor3.logs().blockingFirst();
-
-        if (createdLog3 != null) {
             assertThat(createdLog.getSensorId()).matches(webSensorConfig1.getId());
             assertThat(createdLog.getAttributeValue()).hasSize(1);
         }
