5eca61cee375b537d4d8a97c025bcdb6b8a2ec9b
lkxlaz <lkxlaz@live.com>
2017-10-24 10:39:09 +1100

    fixed Mongodb connection issue

--- pems-server/src/main/java/org/maroubra/pemsserver/configuration/MongoDbConfiguration.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/configuration/MongoDbConfiguration.java
@@ -4,7 +4,7 @@ import com.github.joschi.jadconfig.Parameter;
 
 public class MongoDbConfiguration {
     @Parameter(value = "mongodb_connection")
-    public String connectionString = "mongodb://0.0.0.0:27017";
+    public String connectionString = "mongodb://127.0.0.1:27017";
 
     @Parameter(value = "mongodb_database")
     public String databaseName = "pems";
