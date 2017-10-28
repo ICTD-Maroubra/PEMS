f4a8f9a646f4a822b5bfee1c103867f20544a9c5
Vishal Uniyal <deniss.uniyal@gmail.com>
2017-10-28 16:03:24 +1100

    Find and modify changes in event data

--- pems-server/src/main/java/org/maroubra/pemsserver/api/models/events/responses/EventListResponse.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/api/models/events/responses/EventListResponse.java
@@ -12,6 +12,19 @@ public class EventListResponse {
                 while(i<array.length){
                         if(array[tempMax]> User.TempSlider()){
 
+                                Query query6 = new Query();
+                                query6.addCriteria(Criteria.where("name").is("appleF"));
+                                
+                                Update update6 = new Update();
+                                update6.set("age", 101);
+                                update6.set("ic", 1111);
+                                
+                                //FindAndModifyOptions().returnNew(true) = newly updated document
+                                //FindAndModifyOptions().returnNew(false) = old document (not update yet)
+                                User userTest6 = mongoOperation.findAndModify(
+                                                query6, update6,
+                                                new FindAndModifyOptions().returnNew(true), User.class);
+                                System.out.println("userTest6 - " + userTest6);
                         }
                 }
 
