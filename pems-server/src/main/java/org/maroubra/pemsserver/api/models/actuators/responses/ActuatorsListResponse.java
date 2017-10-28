ccce3f98a18b3450d683d88c28a7f93c03635d9f
lkxlaz <lkxlaz@live.com>
2017-10-29 09:28:36 +1100

    implemented actuators API

--- pems-server/src/main/java/org/maroubra/pemsserver/api/models/actuators/responses/ActuatorsListResponse.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/api/models/actuators/responses/ActuatorsListResponse.java
@@ -1,4 +1,25 @@
 package org.maroubra.pemsserver.api.models.actuators.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.maroubra.pemsserver.control.AbstractActuator;
+import org.maroubra.pemsserver.monitoring.ConfigDescriptor;
+
 public class ActuatorsListResponse {
+
+    @JsonProperty("id")
+    public String id;
+
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("config")
+    public ConfigDescriptor config;
+
+    public static ActuatorsListResponse create(AbstractActuator actuator) {
+        ActuatorsListResponse actuatorsListResponse = new ActuatorsListResponse();
+        actuatorsListResponse.id = String.valueOf(actuator.getId());
+        actuatorsListResponse.name = actuator.getName();
+        actuatorsListResponse.config = null;
+        return actuatorsListResponse;
+    }
 }
--- pems-server/src/main/java/org/maroubra/pemsserver/api/resources/ActuatorsResource.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/api/resources/ActuatorsResource.java
@@ -3,9 +3,14 @@ package org.maroubra.pemsserver.api.resources;
 import io.swagger.annotations.*;
 import org.maroubra.pemsserver.api.models.actuators.requests.UpdateActuatorRequest;
 import org.maroubra.pemsserver.api.models.actuators.responses.ActuatorsListResponse;
+import org.maroubra.pemsserver.control.AbstractActuator;
+import org.maroubra.pemsserver.control.ActuatorFactory;
+import org.maroubra.pemsserver.control.ControlServiceImpl;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "Actuators")
 @Path("/actuators")
@@ -14,8 +19,13 @@ public class ActuatorsResource {
 
     @GET
     @ApiOperation(value = "List actuators")
-    public ActuatorsListResponse listActuators() {
-        throw new UnsupportedOperationException();
+    public List<ActuatorsListResponse> listActuators() {
+        List<ActuatorsListResponse> responses = new ArrayList<>();
+        List<AbstractActuator> actuators = new ControlServiceImpl().listActuators();
+        for (AbstractActuator actuator: actuators) {
+            responses.add(ActuatorsListResponse.create(actuator));
+        }
+        return responses;
     }
 
     @PUT
