93de4503e6c33aba3fb3510cbc2665ca57681cb5
jeremyyiu <utsjeremy@gmail.com>
2017-10-29 12:46:15 +1100

    merged changes with develop branch

--- pems-server/src/main/java/org/maroubra/pemsserver/monitoring/sensortag/AccelerationNotification.java
+++ pems-server/src/main/java/org/maroubra/pemsserver/monitoring/sensortag/AccelerationNotification.java
@@ -2,9 +2,8 @@ package org.maroubra.pemsserver.monitoring.sensortag;
 
 import com.google.common.collect.ImmutableMap;
 import io.reactivex.processors.FlowableProcessor;
+import org.maroubra.pemsserver.monitoring.SensorConfig;
 import org.maroubra.pemsserver.monitoring.SensorLog;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import tinyb.BluetoothNotification;
 
 import java.text.DecimalFormat;
@@ -15,13 +14,12 @@ import java.text.DecimalFormat;
  */
 public class AccelerationNotification implements BluetoothNotification<byte[]> {
 
+    // Configuration for the sensortag that is subscribed to this notification
+    private final SensorConfig config;
 
-    private static final Logger log = LoggerFactory.getLogger(SensortagSensor.class);
-
-    private final SensortagSensor.Config config;
     private final FlowableProcessor<SensorLog> processor;
 
-    public AccelerationNotification(SensortagSensor.Config config, FlowableProcessor<SensorLog> processor) {
+    public AccelerationNotification(SensorConfig config, FlowableProcessor<SensorLog> processor) {
         this.config = config;
         this.processor = processor;
     }
@@ -41,8 +39,6 @@ public class AccelerationNotification implements BluetoothNotification<byte[]> {
         acceleration [0] = Double.parseDouble(df.format(x / scale * -1));
         acceleration [1] = Double.parseDouble(df.format(y / scale));
         acceleration [2] = Double.parseDouble(df.format(z / scale * -1));
-
-        log.info("The acceleration: X: {}g, Y: {}g, Z: {}g", acceleration[0], acceleration[1], acceleration[2]);
 
         SensorLog sensorLog = new SensorLog(config.getId(), ImmutableMap.of("accel_x", acceleration[0], "accel_y", acceleration[1], "accel_z", acceleration[2]));
 
